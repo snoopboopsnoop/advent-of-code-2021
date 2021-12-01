a93f3d8d6cf087f4bde3d39df623f0b83248db0b
snoopboopsnoop <chengwaltj@gmail.com>
2021-12-01 08:49:07 -0800

    Completed Day 1

---
+++ Day 1/AdventDay1Part1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+vector<int> readInput();
+void printVector(vector<int> vector);
+
+int main() {
+    //write input to vector
+    vector<int> input = readInput();
+    int count = 0;
+
+    int last = input[0];
+
+    //increase count if input increases
+    for(int i = 1; i < input.size(); i++) {
+        if(input[i] > last) {
+            count++;
+        }
+        last = input[i];
+    }
+
+    cout << count;
+
+    return 0;
+}
+
+vector<int> readInput() {
+    string line;
+    vector<int> inputVector;
+    ifstream input("input.txt");
+
+    //put each line of input file into vector
+    if(input.is_open()) {
+        while(getline(input, line)) {
+            inputVector.push_back(stoi(line));
+        }
+    }
+    return inputVector;
+}
+
+void printVector(vector<int> vector) {
+    for(int i = 0; i < vector.size(); i++) {
+        cout << vector.at(i) << "\n";
+    }
+}
---
+++ Day 1/AdventDay1Part2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+vector<int> readInput();
+void printVector(vector<int> vector);
+
+int main() {
+    //write input to vector
+    vector<int> input = readInput();
+    int count = 0;
+    int last = 0;
+
+    //first window
+    for(int i = 0; i < 3; i++) {
+        last += input[i];
+    }
+
+    //increase count if input window increases
+    for(int i = 1; i+2 < input.size(); i++) {
+        int window = 0;
+
+        //create window
+        for(int j = 0; j < 3; j++) {    
+            window += input[i+j];
+        }
+        if(window > last) {
+            count++;
+        }
+        last = window;
+    }
+
+    cout << count;
+
+    return 0;
+}
+
+vector<int> readInput() {
+    string line;
+    vector<int> inputVector;
+    ifstream input("input.txt");
+
+    //put each line of input file into vector
+    if(input.is_open()) {
+        while(getline(input, line)) {
+            inputVector.push_back(stoi(line));
+        }
+    }
+    return inputVector;
+}
+
+void printVector(vector<int> vector) {
+    for(int i = 0; i < vector.size(); i++) {
+        cout << vector.at(i) << "\n";
+    }
+}
